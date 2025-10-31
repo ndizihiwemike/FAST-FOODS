fa2140afe5cee654c17c47b78fce134bf4ab7633
ndizihiwemike <142516647+ndizihiwemike@users.noreply.github.com>
2025-10-31 14:55:03 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+// const router = require('./routes/userRoute')
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.use('/',router)
+
+app.listen(3500,() =>{
+    console.log("working on port 4500");
+})
